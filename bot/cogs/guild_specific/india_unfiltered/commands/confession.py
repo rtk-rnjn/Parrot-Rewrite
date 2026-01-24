44165ec45841bd37082cef6abfac4f01b200f8cd
rtk-rnjn <ritik0ranjan+github@gmail.com>
2026-01-24 17:17:32 +0530

    feat: Enhance confession modal and add listener to delete non-embed messages in the confessions channel

--- bot/cogs/guild_specific/india_unfiltered/commands/confession.py
+++ bot/cogs/guild_specific/india_unfiltered/commands/confession.py
@@ -28,11 +28,14 @@ class ConfessionModal(discord.ui.Modal, title="Anonymous Confession"):
             return
 
         await interaction.response.defer(ephemeral=True)
-        random_adjective = interaction.client.assets.random_adjective
-        random_noun = random.choice(interaction.client.assets.nouns)
-        name = f"Confession by {random_adjective.title()} {random_noun.title()}"
+        random_adjective = interaction.client.assets.random_adjective.title()
+        random_noun = random.choice(interaction.client.assets.nouns).title()
+        name = f"{random_adjective} {random_noun}"
         embed = discord.Embed(
-            title=name, description=self.confession.value, color=discord.Color.purple(), timestamp=discord.utils.utcnow()
+            title=f"Confession by {name}",
+            description=self.confession.value,
+            color=discord.Color.purple(),
+            timestamp=discord.utils.utcnow(),
         )
         if isinstance(channel, discord.abc.Messageable):
             await channel.send(embed=embed)
@@ -51,6 +54,26 @@ class ConfessionCommands(commands.Cog):
         """Make an anonymous confession."""
         modal = ConfessionModal()
         await interaction.response.send_modal(modal)
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        """Listen for messages in the confessions channel and delete non-embed messages."""
+        if message.channel.id != CONFESSIONS_CHANNEL_ID:
+            return
+
+        if message.author.bot:
+            return
+
+        if await self.bot.is_owner(message.author):
+            return
+
+        COMMAND = "</confess:1464579894192373903>"
+
+        if not message.embeds:
+            await message.delete(delay=0)
+            await message.channel.send(
+                f"{message.author.mention}, please use the {COMMAND} command to make anonymous confessions.", delete_after=10
+            )
 
 
 async def setup(bot: commands.Bot) -> None:
